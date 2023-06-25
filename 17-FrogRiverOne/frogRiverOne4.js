f94649702ae62adca5fe7596ea43be4a702ffbd6
mmitacc <manuelmitacc@hotmail.com>
2023-06-24 20:29:27 -0500

    finish 17-FrogRiverOne to 100%

--- 17-FrogRiverOne/frogRiverOne4.js
+++ 17-FrogRiverOne/frogRiverOne4.js
@@ -1,40 +1,20 @@
 const frogRiverOne = (X, A) => {
-  const quickSortSingles = (arr) => {
-    if (arr.length > 1) {
-      let pivot = [arr[0]];
-      let arrLeft = [];
-      let arrRight = [];
-      for (i = 1; i < arr.length; i++) {
-        if (arr[i] > pivot[0]) {
-          arrRight.push(arr[i]);
-        } else if (arr[i] < pivot[0]) {
-          arrLeft.push(arr[i]);
-        }
-      }
-      arr = quickSortSingles(arrLeft)
-        .concat(pivot)
-        .concat(quickSortSingles(arrRight));
-    }
-    return arr;
-  };
-  let frogGoal = [];
+  const ruteX = new Set();
+  let frogGoal = -1;
   for (let i = 0; i < A.length; i++) {
-    frogGoal.push(A[i]);
-    if (i >= X - 1) {
-      frogGoal = quickSortSingles(frogGoal);
-      if (frogGoal.length === X) {
-        return i;
-      }
+    if (!ruteX.has(A[i])) {
+      ruteX.add(A[i]);
+    }
+    if (ruteX.size === X) {
+      return (frogGoal = i);
     }
   }
-  if (frogGoal.length < X) {
-    return -1;
-  }
+  return frogGoal;
 };
 
 //---------------------------------------------
 const arr1 = [1];
-const arr2 = [1, 3, 1, 4, 3, 5, 4, 2];
+const arr2 = [1, 3, 4, 2, 3, 5, 4, 2];
 const arr3 = [1, 3, 1, 4, 3, 3, 5, 4, 2, 4];
 //---------------------------------------------
 console.log(frogRiverOne(1, arr1));
